d087bc37f40170eaa190ec0c91d7b7fa2dcb994f
Pratiksha Ajay Gujrani <pratikshagu@cybage.com>
2024-02-26 19:57:49 +0530

    Header Footer SideBar created

--- frontend/src/app/module/home/home.module.ts
+++ frontend/src/app/module/home/home.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { TopNavComponent } from './top-nav/top-nav.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
- 
+    SideBarComponent,
+    TopNavComponent,
+    FooterComponent,
+  
   ],
   imports: [
     CommonModule,
     HomeRoutingModule
-  ]
+  ],
+  exports:[SideBarComponent,TopNavComponent, FooterComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomeModule { }
