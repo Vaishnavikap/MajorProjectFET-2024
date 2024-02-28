dc6d18b5cf9dc2a6a374f126547044735a3d2b4a
Pratiksha Ajay Gujrani <pratikshagu@cybage.com>
2024-02-28 19:07:58 +0530

    Mofified Footer and Updated the Home Component

--- frontend/src/app/module/home/home.module.ts
+++ frontend/src/app/module/home/home.module.ts
@@ -6,18 +6,21 @@ import { SideBarComponent } from './side-bar/side-bar.component';
 import { TopNavComponent } from './top-nav/top-nav.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home/home.component';
+import { RouterModule } from '@angular/router';
+
+
 
 @NgModule({
   declarations: [
     SideBarComponent,
     TopNavComponent,
     FooterComponent,
-    HomeComponent
-  
+    HomeComponent,
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    RouterModule
   ],
   exports:[SideBarComponent,TopNavComponent, FooterComponent, HomeComponent],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
--- frontend/src/app/module/home/top-nav/top-nav.component.ts
+++ frontend/src/app/module/home/top-nav/top-nav.component.ts
@@ -1,12 +1,10 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-nav',
   templateUrl: './top-nav.component.html',
-  styleUrl:'./top-nav.component.css'
+  styleUrls: ['./top-nav.component.css']
 })
-export class TopNavComponent{
-  
-  
+export class TopNavComponent {
 
 }
