dc3a75084ef21218b61fcb4d83ee9c36cef5ae58
vaishnavi <vaishnavika@cybage.com>
2024-02-29 23:55:07 +0530

    Delete controller

--- backend/controller/stripe.controller.js
+++
@@ -1,9 +0,0 @@
-const stripeServices= require("../../backend/ConnectionConfig/connect")
-exports.createPaymentSession= (req, res, next)=>{
-    stripeServices.createSession({priceId:req.body.priceId},(response)=>{
-        return res.status(200).send(response);
-    })
-    
-}
-const express=require('express');
-const router=express.Router();                                   
