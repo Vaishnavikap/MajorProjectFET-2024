581fa21e986e0482e19b2d43d6e6fa7f8687a536
yashlohar24 <153615695+yashlohar24@users.noreply.github.com>
2024-02-29 19:30:20 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './module/home/home.module';
+import { AccordionModule } from './module/accordion/accordion.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { HomeModule } from './module/home/home.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule
+    HomeModule,
+    AccordionModule
   ],
   providers: [
     provideClientHydration()
