7c7caa4f4556237d522296045e05b2fd568cf710
Mosmeepatil <153588679+Mosmeepatil@users.noreply.github.com>
2024-03-04 10:07:19 +0530

    Update app.module.ts

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './module/home/home.module';
-import { RegistrationModule } from './module/registration/registration.module';
+
 
 
 @NgModule({
@@ -14,8 +14,7 @@ import { RegistrationModule } from './module/registration/registration.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HomeModule,
-    RegistrationModule 
+    HomeModule
    
   ],
   providers: [
