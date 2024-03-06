407c2bd2630bc39f15fa25c9f0191f52b7829b38
vaishnavi <vaishnavika@cybage.com>
2024-03-06 11:27:57 +0530

    Chnages in Backend

--- backend/connectionConfig/connect.js
+++ backend/connectionConfig/connect.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose=require("mongoose")
 
 
@@ -13,10 +13,3 @@ mongoose.connect("mongodb+srv://mosmeep:Mosmee%4017@cluster0.0d6hjbd.mongodb.net
 .catch(err => {
   console.error('Error connecting to MongoDB:', err);
 });
-=======
-mongoose.connect('mongodb://localhost:27017/payment', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
->>>>>>> 8ad86ea62112c0169735b111bda31052ad9fc059
